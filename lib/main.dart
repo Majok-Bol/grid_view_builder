e30890ad62507f3a372516bd82febaa2d1674ef7
Majok-Bol <bolmajok001@gmail.com>
2025-03-26 16:43:30 +0300

    changed gridaxis count to 3

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,41 @@ void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
+  final List<String>ImagePath=[
+    "assets/images/p1.jpg",
+    "assets/images/p2.jpg",
+    "assets/images/p3.jpeg",
+        "assets/images/p4.jpeg",
+        "assets/images/p5.jpeg",
+        "assets/images/p6.jpeg",
+        "assets/images/p7.jpeg",
+        "assets/images/p8.jpeg",
+        "assets/images/profile1.webp",
+        "assets/images/profile2.webp",
+         "assets/images/profile3.webp",
+        "assets/images/profile4.webp"
+  ];
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text('GridView.builder Widget',style: TextStyle(color: Colors.white,fontSize: 20),),centerTitle:true ,backgroundColor: Colors.purple,),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          color:Colors.grey,
+         child:Expanded(child: GridView.builder(gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,
+
+           crossAxisSpacing: 10,
+           mainAxisSpacing: 10,
+         ),
+             itemCount: ImagePath.length,
+             itemBuilder: (context,index){
+           return Image.asset(ImagePath[index]);
+         })
+         ),
+        ),
       ),
     );
   }
